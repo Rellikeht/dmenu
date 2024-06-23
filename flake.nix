b0310f3450e405b68a81a917a0624dc77438769c
Rellikeht <michaelhemperek@gmail.com>
2024-06-23 17:57:32 +0200

    little update

--- flake.nix
+++ flake.nix
@@ -2,54 +2,73 @@
   description = "Rellikeht's build of suckless dmenu";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs;
-    flake-utils.url = github:numtide/flake-utils;
-  };
+    # {{{
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  }; # }}}
 
   outputs = {
+    # {{{
     self,
     nixpkgs,
     flake-utils,
   }:
-    flake-utils.lib.eachSystem [
-      "x86_64-linux"
-      "i686-linux"
-      "aarch64-linux"
-      "armv7l-linux"
-    ] (system: let
+  # }}}
+    flake-utils.lib.eachSystem
+    flake-utils.lib.allSystems
+    # [
+    #   "x86_64-linux"
+    #   "i686-linux"
+    #   "aarch64-linux"
+    #   "armv7l-linux"
+    # ]
+    (system: let
+      # {{{
       pkgs = nixpkgs.legacyPackages.${system};
       name = "dmenu";
       src = self;
+      # }}}
     in {
       packages.default = pkgs.stdenv.mkDerivation rec {
         inherit name system src;
+
+        # {{{
         PREFIX = "$(out)";
         CC = pkgs.gcc;
+        # }}}
 
         buildInputs = with pkgs; [
+          # {{{
           xorg.libX11
           xorg.libXft
           xorg.libXinerama
-        ];
+        ]; # }}}
 
-        buildPhase = ''
-          make clean
-          make
-        '';
+        buildPhase =
+          # {{{
+          ''
+            make clean
+            make
+          '';
+        # }}}
 
-        installPhase = ''
-          mkdir -p $out/bin
-          make install
-        '';
+        installPhase =
+          # {{{
+          ''
+            mkdir -p $out/bin
+            make install
+          '';
+        # }}}
 
         meta = with pkgs.lib; {
+          # {{{
           homepage = "https://tools.suckless.org/dmenu";
           description = "Suckless dmenu, Rellikeht's build";
           license = licenses.mit;
           mainProgram = "dmenu";
           maintainers = ["Rellikeht"];
           platforms = platforms.linux;
-        };
+        }; # }}}
       };
     });
 }
