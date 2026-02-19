7486a120b5be4eb410f79a6dbe9ed5269e05a21f
Rellikeht <michaelhemperek@gmail.com>
2026-02-19 14:34:10 +0100

    fix pointer trickery

--- config.h
+++ config.h
@@ -5,20 +5,21 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
+static char font[256] = "MesloLGS:size=12:antialias=true:autohint=true";
 static const char *fonts[] = {
-    "MesloLGS NF:size=12:antialias=true:autohint=true",
+    font,
     // All possible combinations just in case
-    "MesloLGS NF:size=12:antialias=true:autohint=true",
-    "MesloLGS Nerd FontF:size=12:antialias=true:autohint=true",
     "MesloLGS:size=12:antialias=true:autohint=true",
     "MesloLGS:size=12",
-    "MesloLGS NF:size=12",
-    "MesloLGS Nerd FontF:size=12",
-    "MesloLGS:size=12",
+    "MesloLG:size=12:antialias=true:autohint=true",
+    "MesloLG:size=12",
+    "Liberation Mono:size=12:antialias=true:autohint=true",
+    "Liberation Mono:size=12",
     "monospace:size=12",
 };
 
-static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static char prompt_buf[256] = {'\0'};
+static char *prompt = prompt_buf; /* -p  option; prompt to the left of input field */
 
 static char normfgcolor[] = "#086aab";
 static char normbgcolor[] = "#01080b";
@@ -54,14 +55,14 @@ static const char worddelimiters[] = " ";
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-	{ "font",           STRING, &fonts[0] },
-	{ "normfgcolor",    STRING, &normfgcolor },
-	{ "normbgcolor",    STRING, &normbgcolor },
-	{ "selfgcolor",     STRING, &selfgcolor },
-	{ "selbgcolor",     STRING, &selbgcolor },
-	{ "hlnormfgcolor",  STRING, &hlnormfgcolor },
-	{ "hlnormbgcolor",  STRING, &hlnormbgcolor },
-	{ "hlselfgcolor",   STRING, &hlselfgcolor },
-	{ "hlselbgcolor",   STRING, &hlselbgcolor },
-	{ "prompt",         STRING, &prompt },
+	{ "font",           STRING, font },
+	{ "normfgcolor",    STRING, normfgcolor },
+	{ "normbgcolor",    STRING, normbgcolor },
+	{ "selfgcolor",     STRING, selfgcolor },
+	{ "selbgcolor",     STRING, selbgcolor },
+	{ "hlnormfgcolor",  STRING, hlnormfgcolor },
+	{ "hlnormbgcolor",  STRING, hlnormbgcolor },
+	{ "hlselfgcolor",   STRING, hlselfgcolor },
+	{ "hlselbgcolor",   STRING, hlselbgcolor },
+	{ "prompt",         STRING, prompt_buf },
 };
